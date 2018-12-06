80fa0648155d8e07402b31cc490409a8ee91a188
baichenhao <1441448530@qq.com>
2018-12-06 09:41:21 +0800

    123

--- runner/src/com/jerrymice/runner/map/dao/MapDao.java
+++
@@ -1,9 +0,0 @@
-package com.jerrymice.runner.map.dao;
-
-
-import com.jerrymice.runner.entity.Map;
-import com.jerrymice.runner.util.dao.BaseDao;
-
-public interface MapDao extends BaseDao<Map> {
-
-}
--- runner/src/com/jerrymice/runner/map/dao/MapDaoImpl.java
+++
@@ -1,9 +0,0 @@
-package com.jerrymice.runner.map.dao;
-
-
-import com.jerrymice.runner.entity.Map;
-import com.jerrymice.runner.util.impl.BaseDaoImpl;
-
-public class MapDaoImpl extends BaseDaoImpl<Map> implements MapDao {
-
-}
