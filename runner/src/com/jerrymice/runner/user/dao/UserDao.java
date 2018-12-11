add8858b6d006b42a4ee6eb0937deba287d8e1ba
maolinxin1997 <530902887@qq.com>
2018-12-11 15:17:55 +0800

    1

--- runner/src/com/jerrymice/runner/user/dao/UserDao.java
+++
@@ -1,8 +0,0 @@
-package com.jerrymice.runner.user.dao;
-
-import com.jerrymice.runner.entity.User;
-import com.jerrymice.runner.util.dao.BaseDao;
-
-public interface UserDao extends BaseDao<User> {
-
-}
